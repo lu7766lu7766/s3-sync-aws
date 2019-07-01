baa0191807ca0423b66cef7658de65edbfd17b4b
Jac <lu7766lu7766@gmail.com>
2019-07-01 17:37:19 +0800

    包裝成function

--- index.js
+++ index.js
@@ -1,211 +1,40 @@
-var LevelWriteStream = require('level-write-stream')
-  , createQueue = require('queue-async')
-  , backoff = require('backoff')
-  , es = require('event-stream')
-  , crypto = require('crypto')
-  , xtend = require('xtend')
-  , mime = require('mime')
-  , once = require('once')
-  , AWS = require('aws-sdk')
-  , url = require('url')
-  , fs = require('fs')
+var s3sync = require('./s3sync.js')
 
-module.exports = s3syncer
+var path = require('path'),
+	level = require('level'),
+	readdirp = require('readdirp'),
+	moment = require('moment'),
+	es = require('event-stream')
 
-function s3syncer(db, options) {
-  if (!options) {
-    options = db || {}
-    db = false
-  }
+var resolve = dir => {
+	return path.join(process.cwd(), dir)
+}
 
-  options.concurrency = options.concurrency || 16
-  options.headers = options.headers || {}
-  options.cacheSrc = options.cacheSrc || __dirname + '/.sync'
-  options.cacheDest = options.cacheDest || '/.sync'
-  options.retries = options.retries || 7
-  options.acl = options.acl || 'public-read'
-  options.force = !!options.force
-  options.accessKeyId = options.accessKeyId || options.key
-  options.secretAccessKey = options.secretAccessKey || options.secret
-  options.dest = options.dest || ''
+module.exports = async (dir, options) => {
+	var db = level(resolve(dir + '/cache'))
 
-  var dest = options.dest ? (options.dest + '/').replace('//', '/') : ''
+	var files = readdirp(resolve(dir), {
+		directoryFilter: ['!.git', '!cache']
+	})
 
-  var client = new AWS.S3(options)
-    , queue = createQueue(options.concurrency)
-    , region = options.region === 'us-standard' ? false : options.region
-    , secure = options.secure || !('secure' in options)
-    , subdomain = region ? 's3-' + region : 's3'
-    , protocol = secure ? 'https' : 'http'
-    , prefix = options.prefix || ''
-    , hashkey = options.hashKey || function(details) {
-      return details.fullPath
-    }
+	let index = 0
+	var conf = {
+		key: options.access,
+		secret: options.secret,
+		bucket: options.bucket,
+		concurrency: 100
+	}
+	options.dir && (conf.dest = options.dir)
 
-  var stream = es.map(function(data, next) {
-    queue.defer(function(details, done) {
-      details.fullPath = details.fullPath || details.src
-      details.path = details.path || details.dest
-      syncFile(details, function(err) {
-        return err ? next(err) : done(), next(null, details)
-      })
-    }, data)
-  })
+	var uploader = s3sync(db, conf).on('data', function(file) {
+		if (file.fresh) {
+			console.log(`${file.fullPath}\n->${file.url}\nindex:${index++}\n`)
+		}
+	})
 
-  stream.getCache = getCache
-  stream.putCache = putCache
-
-  function syncFile(details, next) {
-    var absolute = details.fullPath
-      , relative = prefix + (details.path.charAt(0) === '/'
-        ? details.path.slice(1)
-        : details.path)
-    
-
-    relative = relative.replace(/\\/g, '/')
-
-    var destination =
-          protocol + '://'
-        + subdomain
-        + '.amazonaws.com/'
-        + options.bucket + '/'
-        + dest 
-        + relative
-
-    hashFile(absolute, destination, function(err, md5) {
-      if (err) return next(err)
-      details.md5 = md5
-      details.url = destination
-      details.fresh = false
-      details.cached = false
-
-      if (!db) return checkForUpload(next)
-
-      var key = 'md5:' + hashkey(details)
-
-      db.get(key, function(err, result) {
-        if (!err && result === md5) {
-          details.cached = true
-          return next(null, details)
-        }
-        checkForUpload(function(err) {
-          if (err) return next(err)
-          db.put(key, md5, next)
-        })
-      })
-    })
-
-    function checkForUpload(next) {
-      client.headObject({Bucket: options.bucket, Key: relative}, function(err, res) {
-        if (err && err.statusCode !== 404) return next(err)
-        if (err && err.statusCode === 404) return uploadFile(details, next)
-        if (
-          options.force || (
-          res.Metadata['syncfilehash'] !== details.md5
-        )) return uploadFile(details, next)
-        return next(null, details)
-      })
-    }
-  }
-
-  function uploadFile(details, next) {
-    var absolute = details.fullPath
-      , relative = prefix + details.path
-      , lasterr
-      , off = backoff.fibonacci({
-        initialDelay: 1000
-      })
-
-    relative = dest + relative.replace(/\\/g, '/')
-    details.fresh = true
-
-    off.failAfter(options.retries)
-    off.on('fail', function() {
-      next(lasterr || new Error('unknown error'))
-    }).on('ready', function() {
-      var params = xtend({
-        Bucket: options.bucket,
-        Key: relative,
-        ContentType: mime.lookup(absolute),
-        ACL: options.acl,
-        Metadata: {
-          syncfilehash: details.md5
-        },
-        Body: fs.createReadStream(absolute)
-      }, options.headers)
-
-      client.putObject(params, function(err, res) {
-        if (err) {
-          err = new Error('Bad status code: ' + err.statusCode)
-        } else {
-          return next(null, details)
-        }
-
-        lasterr = err
-        stream.emit('fail', err)
-        off.backoff()
-      })
-    }).backoff()
-  }
-
-  function getCache(callback) {
-    callback = once(callback)
-
-    client.getObject({
-      Bucket: options.bucket,
-      Key: options.cacheDest
-    }, function(err, res) {
-      if (err && err.statusCode !== 404) return callback(err)
-      if (err && err.statusCode === 404) return callback(null)
-
-      es.pipeline(
-          res
-        , es.split()
-        , es.parse()
-        , LevelWriteStream(db)()
-      ).once('close', callback)
-       .once('error', callback)
-    })
-  }
-
-  function putCache(callback) {
-    callback = once(callback)
-
-    db.createReadStream()
-      .pipe(es.stringify())
-      .pipe(fs.createWriteStream(options.cacheSrc))
-      .once('error', callback)
-      .once('close', function() {
-        client.putObject({
-          Bucket: options.bucket,
-          Key: options.cacheDest,
-          Body: fs.createReadStream(options.cacheSrc)
-        }, function(err) {
-          if (err) return callback(err)
-          fs.unlink(options.cacheSrc, callback)
-        })
-     })
-  }
-
-  function hashFile(filename, destination, callback) {
-    var hash = crypto.createHash('md5')
-      , done = false
-
-    hash.update(JSON.stringify([
-        options.headers
-      , destination
-    ]))
-
-    fs.createReadStream(filename).on('data', function(d) {
-      hash.update(d)
-    }).once('error', function(err) {
-      if (!done) callback(err)
-      done = true
-    }).once('close', function() {
-      if (!done) callback(null, hash.digest('hex'))
-      done = true
-    })
-  }
-
-  return stream
+	files.pipe(uploader).pipe(
+		es.wait(function(err, body) {
+			console.log('start_time: ' + moment().format('YYYY-MM-DD HH:mm:ss'))
+		})
+	)
 }
